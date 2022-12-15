911157903ffdad0cb2c91a1d29f5a8d83f01f89d
Damien Dotta <damien.dotta@live.fr>
2022-12-15 18:02:16 +0100

    Add asserts functions

---
+++ R/asserts.R
@@ -0,0 +1,64 @@
+# Assertions for parameter validates (from openxlsx package)
+# These should be used at the beginning of functions to stop execution early
+
+#' @name assert_class
+#'
+#' @param x Object
+#' @param class class to test
+#' @param or_null boolean. By default FALSE
+#'
+#' @noRd
+assert_class <- function(x, class, or_null = FALSE) {
+  sx <- as.character(substitute(x))
+  ok <- inherits(x, class)
+
+  if (or_null) {
+    ok <- ok | is.null(x)
+    class <- c(class, "null")
+  }
+
+  if (!ok) {
+    msg <- sprintf("%s must be of class %s", sx, paste(class, collapse = " or "))
+    stop(msg, call. = FALSE)
+  }
+}
+
+#' @name assert_character1
+#'
+#' @param x Object
+#' @param scalar boolean. By default FALSE
+#'
+#' @noRd
+assert_character1 <- function(x, scalar = FALSE) {
+  ok <- is.character(x) && length(x) == 1L
+
+  if (scalar) {
+    ok <- ok & nchar(x) == 1L
+  }
+
+  if (!ok) {
+    stop(substitute(x), " must be a character vector of length 1L", call. = FALSE)
+  }
+}
+
+#' @name assert_numeric1
+#'
+#' @param x Object
+#' @param scalar boolean. By default FALSE
+#'
+#' @noRd
+assert_numeric1 <- function(x, scalar = FALSE) {
+  msg <- paste0(substitute(x), " must be a ")
+  ok <- is.numeric(x) & length(x) == 1L
+
+  if (scalar) {
+    ok <- ok && nchar(x) == 1L
+    msg <- paste0(msg, "single number")
+  } else {
+    msg <- paste0(msg, "numeric vector of length 1L")
+  }
+
+  if (!ok) {
+    stop(msg, call. = FALSE)
+  }
+}
