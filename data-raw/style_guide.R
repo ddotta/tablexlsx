fd1f04eca3a28a1577935131a12540e8fb247fde
Damien Dotta <damien.dotta@live.fr>
2022-12-19 16:29:41 +0100

    Update style_guide

--- data-raw/style_guide.R
+++ data-raw/style_guide.R
@@ -5,20 +5,47 @@
 ## Style's definitions
 style_guide <- list(
   # For title
-  style_title <- openxlsx::createStyle(fontSize = 16, textDecoration = c("bold")),
+  style_title = openxlsx::createStyle(fontSize = 16, textDecoration = c("bold")),
   # For footnote1
-  style_footnote1 <- openxlsx::createStyle(fontSize = 12),
+  style_footnote1 = openxlsx::createStyle(fontSize = 12),
   # For footnote2
-  style_footnote2 <- openxlsx::createStyle(fontSize = 12, textDecoration = c("bold")),
+  style_footnote2 = openxlsx::createStyle(fontSize = 12, textDecoration = c("bold")),
   # For footnote3
-  style_footnote3 <- openxlsx::createStyle(fontSize = 12, textDecoration = c("bold")),
+  style_footnote3 = openxlsx::createStyle(fontSize = 12, textDecoration = c("bold")),
   # For column headers
-  style_col_header <- openxlsx::createStyle(fontSize = 12,
+  style_col_header = openxlsx::createStyle(fontSize = 12,
                                             textDecoration = c("bold"),
                                             border = c("top", "bottom", "left", "right"),
                                             borderStyle = "thin",
                                             wrapText = TRUE,
-                                            halign = "center")
+                                            halign = "center"),
+
+  # For column headers
+  style_col_header = openxlsx::createStyle(fontSize = 12,
+                                              textDecoration = c("bold"),
+                                              border = c("top", "bottom", "left", "right"),
+                                              borderStyle = "thin",
+                                              wrapText=TRUE,
+                                              halign = "center"),
+
+  # For cells in character format
+  char = openxlsx::createStyle(fontSize = 12,
+                                  border = c("top", "bottom", "left", "right"),
+                                  borderStyle = "thin"),
+
+  # For cells in number format (with thousands separator)
+  number = openxlsx::createStyle(fontSize = 12,
+                                      numFmt = "### ### ### ##0",
+                                      border = c("top", "bottom", "left", "right"),
+                                      borderStyle = "thin"),
+
+  # For cells in percentage format (centered)
+  percent = openxlsx::createStyle(fontSize = 12,
+                                        numFmt = "#0.0",
+                                        border = c("top", "bottom", "left", "right"),
+                                        borderStyle = "thin",
+                                        halign = "center")
+
 )
 
 
