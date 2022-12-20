dcbade5b3095c152d9c2a97e80c75c59605882c2
Damien Dotta <damien.dotta@live.fr>
2022-12-20 10:57:56 +0100

    Add first draft of column_style()

---
+++ R/column_style.R
@@ -0,0 +1,12 @@
+column_style <- function(object,ListStyle) {
+
+  # check if ListStyle is a list
+  assert_class(ListStyle, "list")
+  # check if ListStyle is a named list
+  assert_named_list(ListStyle)
+  # check if ListStyle is a list composed of lists of which all elements are named
+  assert_named_list_in_list(ListStyle)
+
+  return(ListStyle)
+
+}
