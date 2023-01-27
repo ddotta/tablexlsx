67a5ce665bbf09e0f701dd3b2e9dbe03fc73108e
Damien Dotta <damien.dotta@live.fr>
2023-01-27 15:02:08 +0100

    Add documentation for style object

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' An R list that contains the styles of each element for formatting data frames in excel files
+#'
+#' @format A list of several items
+#' \describe{
+#' \item{title}{Style for title}
+#' \item{footnote1}{Style for footnote1}
+#' \item{footnote2}{Style for footnote2}
+#' \item{footnote3}{Style for footnote3}
+#' \item{col_header}{Style for col_header}
+#' \item{character}{Style for columns in character format}
+#' \item{number}{Style for columns in number format}
+#' \item{decimal}{Style for columns in character format with decimals}
+#' \item{percent}{Style for columns in percent format}
+#' }
+"style"
+
