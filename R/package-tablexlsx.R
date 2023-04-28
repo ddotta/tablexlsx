d14270af439b4a983a7ce66b44bfedb9d272cce4
ddotta <damien.dotta@live.fr>
2023-04-28 10:09:59 +0200

    docs: new R script package-tablexlsx

---
+++ R/package-tablexlsx.R
@@ -0,0 +1,8 @@
+#' @keywords internal
+#' @importFrom dplyr is.grouped_df
+#' @importFrom magrittr "%>%"
+#' @importFrom methods allNames
+#' @importFrom openxlsx addStyle addWorksheet mergeCells openXL saveWorkbook setColWidths setRowHeights writeData writeDataTable
+#' @importFrom stats setNames
+#' @import utils
+"_PACKAGE"
